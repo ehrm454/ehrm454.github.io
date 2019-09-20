c11d6dd2d694d6034ad7b4697ac8d34d6249af3d
ehrm454 <37520319+ehrm454@users.noreply.github.com>
2019-09-20 13:00:29 +0900

    Add files via upload

--- html_p/js/js.js
+++ html_p/js/js.js
@@ -1,65 +1,61 @@
 $(document).ready(function () {
+    var height =$('.banner_out_left').css("height");
+    $(".banner_out_left img").css("max-height",height);
+    var ppt_height= $(".ppt_img2 img").css("height");
+    $(".ppt_contents").css("height",ppt_height);
+    $('#banner_img1 img , #banner_img2 img , #banner_img3 img , .intro_me , .skill_intro').hide();
 
-    $(window).scroll(function () {
-        var scrollValue = $(document).scrollTop();
-        console.log(scrollValue);
+    $('.hover_1').hover(function () {
+    });
+    $('.hover_2').hover(function () {
+        $('.intro_me').show(500);
+    });
+    $('.hover_3').hover(function () {
+     $('#skills , .skill_intro').show(500);
+
+    });
+    $('.hover_4').hover(function () {
+/*        $('#banner_img1').css("z-index","90");*/
+        $('#banner_img1 img').show(500);
+    });
+    $('.hover_5').hover(function () {
+   /*     $('#banner_img2').css("z-index","90");*/
+        $('#banner_img2 img').show(500);
+    });
+    $('.hover_6').hover(function () {
+    /*    $('#banner_img3').css("z-index","90");*/
+        $('#banner_img3 img').show(500);
     });
 
-    var left = $(".me_intro");
-    var right = $(".skill_intro");
-    var img1 = $(".ppt_img1");
-    var img2 = $(".ppt_img2");
-    var img3 = $(".ppt_img3");
 
-    right.hide();
-    $(img1).css("left", "-600px");
-    $(img3).css("left", "-600px");
-
-    $(left).css("top", "-700px");
-    $(left).css("left", "-300px");
-
-    $(right).css("top", "-1000px");
-    $(right).css("right", "-300px");
-
-    $(window).on("scroll", function () {
-        let top = $(window).scrollTop();
-        if (top >= 545) {
-            right.show();
-            setTimeout(function () {
-                $(left).animate({
-                    bottom: 0, left: 0, top: 0, right: 0
-                }, 1000);
-                $(right).animate({
-                    bottom: 0, left: 0, top: 0, right: 0
-                }, 1000);
-            });
-        }
-        if (top >= 1500) {
-            $(img2).css("right", "-700px");
-            setTimeout(function () {
-                $(img1).animate({
-                    bottom: 0, left: 0, top: 0, right: 0
-                }, 700);
-            });
-        }
-
-        if (top >= 2410) {
-
-            setTimeout(function () {
-                $(img2).animate({
-                    bottom: 0, left: 0, top: 0, right: 0
-                }, 700);
-
-            });
-        }
-
-        if (top >= 3600) {
-            setTimeout(function () {
-                $(img3).animate({
-                    bottom: 0, left: 0, top: 0, right: 0
-                }, 700);
-            });
-
-        }
+    $('.hover_1').mouseleave(function () {
     });
+    $('.hover_2').mouseleave(function () {
+        $('.intro_me').hide(500);
+    });
+    $('.hover_3').mouseleave(function () {
+        $('#skills').hide(500);
+    });
+    $('.hover_4').mouseleave(function () {
+        $('#banner_img1 img').hide(500);
+
+    });
+    $('.hover_5').mouseleave(function () {
+        $('#banner_img2 img').hide(500);
+    });
+    $('.hover_6').mouseleave(function () {
+        $('#banner_img3 img').hide(500);
+    });
+
+
+    $('nav a, .top > a ').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+
+
+
+
 });
